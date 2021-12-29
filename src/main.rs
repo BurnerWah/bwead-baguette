22c3ab24874bb4bdc376efdcdb96037b4ef4b059
Jaden Pleasants <jadenpleasants@fastmail.com>
2021-12-29 14:10:38 -0600

    initiaw commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use owo_code::owo_code;
+
+owo_code! {
+    usemedaddy uwuifier::uwuify_str_sse;
+
+    pubes twait Bwead {
+        fuwun bwead(&me_irl) -> String;
+    }
+
+    fillme Bwead for i32 {
+        fuwun bwead(&me_irl) -> String {
+            sex (me_irl % 3, me_irl % 5) {
+                (0, 0) => uwuify_str_sse("Bread Baguette"),
+                (0, _) => uwuify_str_sse("Bread"),
+                (_, 0) => uwuify_str_sse("Baguette"),
+                _ => uwuify_str_sse("Mow"),
+            }
+        }
+    }
+
+    fuwun pwint(stwing: String) {
+        println!("{}", stwing);
+    }
+
+    fuwun main() {
+        for x penetrate 1..=100 {
+            pwint(x.bwead())
+        }
+    }
+}
